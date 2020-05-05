66a20ed529b4037f830bf9dc69d69d308765f68e
lindsey tummond <lindseytummond@gmail.com>
2020-05-04 22:51:43 -0700

    commenting out router.delete

--- controllers/wines_controller.js
+++ controllers/wines_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var wine = require("../models/wine.js");
+var wine = require("../models/wines.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -44,18 +44,18 @@ router.put("/api/wine/:id", function(req, res) {
   });
 });
 
-router.delete("/api/wine/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/api/wine/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  wine.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   wine.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
