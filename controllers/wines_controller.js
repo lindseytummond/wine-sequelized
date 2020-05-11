02820550f4159d0b304dbd2dec9273ce6a70f2f6
lindsey tummond <lindseytummond@gmail.com>
2020-05-11 12:24:27 -0700

    updating wines_controller with sequelize routes

--- controllers/wines_controller.js
+++ controllers/wines_controller.js
@@ -3,44 +3,93 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var wine = require("../models/wine.js");
+var db = require("../models/wine.js");
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  wine.all(function(data) {
-    var hbsObject = {
-      wines: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+// // Create all our routes and set up logic within those routes where required.
+// router.get("/", function(req, res) {
+//   wine.all(function(data) {
+//     var hbsObject = {
+//       wines: data
+//     };
+//     console.log(hbsObject);
+//     res.render("index", hbsObject);
+//   });
+// });
+
+// get route -> index
+router.get("/", function (req, res) {
+  // send us to the next get function instead.
+  res.redirect("/wines");
 });
 
-router.post("/api/wines", function(req, res) {
-  wine.create([
-    "name", "taste"
-  ], [
-    req.body.name, req.body.taste
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+// get route for sequelize
+router.get("/wines", function (req, res) {
+  db.Wine.findAll()
+    .then(function (dbWine) {
+      console.log(dbWine);
+      // into the main index, updating the page
+      var hbsObject = { wine: dbWine };
+      return res.render("index", hbsObject);
+    });
 });
 
-router.put("/api/wines/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+// router.post("/api/wines", function(req, res) {
+//   wine.create([
+//     "name", "taste"
+//   ], [
+//     req.body.name, req.body.taste
+//   ], function(result) {
+//     // Send back the ID of the new quote
+//     res.json({ id: result.insertId });
+//   });
+// });
 
-  wine.update({
-    taste: req.body.taste
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
+// get route for sequelize
+router.post("/wines/create", function (req, res) {
+  // edit create for new wine
+  db.Wine.create({
+    wine_name: req.body.wine_name
+  })
+    // pass the result of our call
+    .then(function (dbWine) {
+      console.log(dbWine);
+      // redirect
+      res.redirect("/");
+    });
+});
+
+
+// router.put("/api/wines/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   console.log("condition", condition);
+
+//   wine.update({
+//     taste: req.body.taste
+//   }, condition, function(result) {
+//     if (result.changedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
+
+// put route for sequelize
+router.put("/wines/update/:id", function (req, res) {
+  // update wine
+  db.Wine.update({
+    taste: true
+  },
+    {
+      where: {
+        id: req.params.id
+      }
     }
+  ).then(function (dbWine) {
+    res.json("/");
   });
 });
 
